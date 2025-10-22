e4582e9bd0ed2e90d2b787a7f7329c908404061e
makara-tsc <makara.phuoy@techostartup.center>
2025-10-22 10:29:21 +0700

    Add content policy headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`;
 const nextConfig = {
   reactStrictMode: true,
   eslint: {
@@ -13,6 +25,19 @@ const nextConfig = {
   // experimental: {
   //  cacheComponents: true
   // },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+        ],
+      }
+    ]
+  }
 }
 
 export default nextConfig
