ee386789cabca3357d97ea8219a6c61308ff2811
makara-tsc <makara.phuoy@techostartup.center>
2025-10-22 10:35:49 +0700

    fix error

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,25 @@
 /** @type {import('next').NextConfig} */
 const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline';
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data:;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    upgrade-insecure-requests;
+  default-src 'self';
+  script-src 
+    'self' 
+    'unsafe-eval' 
+    'unsafe-inline' 
+    https://static.cloudflareinsights.com 
+    https://challenges.cloudflare.com;
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' blob: data:;
+  font-src 'self';
+  object-src 'none';
+  base-uri 'self';
+  form-action 'self';
+  frame-ancestors 'none';
+  frame-src https://challenges.cloudflare.com;
+  connect-src 
+    'self' 
+    https://*.cloudflareinsights.com 
+    https://challenges.cloudflare.com;
+  upgrade-insecure-requests;
 `;
 const nextConfig = {
   reactStrictMode: true,
